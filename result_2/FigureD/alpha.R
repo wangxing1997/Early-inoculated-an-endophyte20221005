741f16fc5bb7922fde068e4a50d2e9c8667d7ace
wangxing1997 <60603417+wangxing1997@users.noreply.github.com>
2022-10-08 15:57:43 +0800

    Update alpha.R

--- result_2/FigureD/alpha.R
+++ result_2/FigureD/alpha.R
@@ -1,5 +1,6 @@
-#Result1,Figuer D,alpha_diversity
+#Result2, Figuer D, alpha_diversity
 #Author: Xing Wang
+# Data: October 8th 2022
 
 getwd()
 rm(list = ls())
@@ -32,17 +33,12 @@ ggsave(paste("stem.pdf", sep=""), p, width = 3, height = 4)
 p
 ggsave(paste("stem.png", sep=""), p, width = 3, height = 4)
 
-# 统计组间是否显著差异
-# anova对指数与分组统计
+
 Shannon_stats <- aov(Shannon ~ Anova, data = df)
 summary(Shannon_stats)
-# 使用TukeyHSD对组间进行检验，效正pvalue
 Tukey_HSD_Shannon <- TukeyHSD(Shannon_stats, ordered = FALSE, conf.level = 0.95)
-# 结果中提取需要的结果
 Tukey_HSD_Shannon_table <- as.data.frame(Tukey_HSD_Shannon$Anova)
-# 预览结果
 Tukey_HSD_Shannon_table
-# 保存结果到文件，按Pvaule值由小到大排序
 write.table(Tukey_HSD_Shannon_table[order(Tukey_HSD_Shannon_table$p, decreasing=FALSE), ], file="stem_stat.txt",append = FALSE, quote = FALSE, sep="\t",eol = "\n", na = "NA", dec = ".", row.names = TRUE,col.names = TRUE)
 
 
