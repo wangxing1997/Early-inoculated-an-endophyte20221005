3ea261412dabdd05f4607e831048883f8a3f11f2
wangxing1997 <60603417+wangxing1997@users.noreply.github.com>
2022-10-08 16:05:34 +0800

    Update iCAMP.R

--- result_5/FigureA/iCAMP.R
+++ result_5/FigureA/iCAMP.R
@@ -1,14 +1,17 @@
+# Result5, Figuer A, iCAMP
+# Author: Xing Wang
+# Data: October 8th 2022
+
 rm(list=ls(all=TRUE))
 library(vegan)
 
-#长数据变宽数据
 rm(list=ls(all=TRUE))
 library(dplyr)
 library(reshape2)
-setwd("C:/Users/liujiale/Desktop")
+setwd()
 
-importance <- read.table("C:/Users/liujiale/Desktop/Pd.txt", sep="\t", header=T)
-group <- read.table("C:/Users/liujiale/Desktop/treatment.txt",sep="\t", header=T, row.names=1)
+importance <- read.table("Pd.txt", sep="\t", header=T)
+group <- read.table("treatment.txt",sep="\t", header=T, row.names=1)
 
 data = transform(importance[,2:3], Selection = importance$Heterogeneous.Selection + importance$Homogeneous.Selection,
                  Dispersal = importance$Dispersal.Limitation + importance$Homogenizing.Dispersal,
@@ -41,15 +44,15 @@ theme_zg <- function(..., bg='white'){
           plot.margin=unit(rep(0.5,4), 'lines'),
           panel.background=element_rect(fill='transparent',color='black'),
           panel.border=element_rect(fill='transparent', color='transparent'),
-          panel.grid=element_blank(),#去网格线
+          panel.grid=element_blank(),
           axis.line = element_line(colour = "black"),
-          axis.title.x=element_text(face = "plain",size = 25),#去x轴标签
-          axis.title.y=element_text(face = "plain",size = 25),#y轴标签加粗及字体大小
-          axis.text = element_text(face = "plain",size = 25),#坐标轴刻度标签加粗          
+          axis.title.x=element_text(face = "plain",size = 25),
+          axis.title.y=element_text(face = "plain",size = 25),
+          axis.text = element_text(face = "plain",size = 25),          
           axis.ticks = element_line(color='black'),
           # axis.ticks.margin = unit(0.8,"lines"),
           legend.title=element_blank(),
-          #legend.position=c(0.85, 0.8),#图例在绘图区域的位置
+          #legend.position=c(0.85, 0.8),
           #legend.direction = "horizontal",
           legend.text = element_text(face = "bold",size = 25,margin = margin(r=8)),
           legend.background = element_rect( linetype="solid",colour ="black")
@@ -57,28 +60,28 @@ theme_zg <- function(..., bg='white'){
 }
 
 P = ggplot(plotdata, aes(developmental_stages,  Value*100, color = Process)) + geom_point(alpha = 0.5, size= 3) + geom_smooth(method = "lm",se=T,size=3) +
-  guides(fill=guide_legend(title=NULL)) + #去除图例背景
+  guides(fill=guide_legend(title=NULL)) + 
   scale_color_manual(values = c("#6495ED", "#FFA500", "#FF4500"))+ 
   facet_wrap(.~ Process, 'free')+
-  stat_poly_eq(aes(label = paste( stat(adj.rr.label), stat(p.value.label),sep = '~~~~')), formula = y ~ x,  #添加回归方程和调整R方
+  stat_poly_eq(aes(label = paste( stat(adj.rr.label), stat(p.value.label),sep = '~~~~')), formula = y ~ x,  
                size = 6,
-               label.x = 0.2,  #位置 ，0-1之间的比例
+               label.x = 0.2,  
                label.y = "top", parse = T, color = 'black') + 
   theme_zg()+ 
-  theme(axis.text.x=element_text(colour="black",family="Times",size=25), #设置x轴刻度标签的字体显示倾斜角度为15度，并向下调整1(hjust = 1)，字体簇为Times大小为25
-        axis.text.y=element_text(family="Times",size=25,face="plain",colour="black"), #设置y轴刻度标签的字体簇，字体大小，字体样式为plain
+  theme(axis.text.x=element_text(colour="black",family="Times",size=25), 
+        axis.text.y=element_text(family="Times",size=25,face="plain",colour="black"),
         strip.text.x = element_text(size=25), strip.text.y = element_text(size=25),
-        axis.title.y=element_text(family="Times",size = 25,face="plain"), #设置y轴标题的字体属性
-        panel.border = element_blank(),axis.line = element_line(colour = "black",size=1), #去除默认填充的灰色，并将x=0轴和y=0轴加粗显示(size=1)
-        legend.text=element_text(face="italic", family="Times", colour="black",  #设置图例的子标题的字体属性
+        axis.title.y=element_text(family="Times",size = 25,face="plain"), 
+        panel.border = element_blank(),axis.line = element_line(colour = "black",size=1), 
+        legend.text=element_text(face="italic", family="Times", colour="black", 
         ),legend.position= c(1,0),legend.justification = c(1,0),legend.background = element_blank(),legend.key.size = unit(12, "pt"),
-        # legend.title=element_text(face="italic", family="Times", colour="black", #设置图例的总标题的字体属性
+        # legend.title=element_text(face="italic", family="Times", colour="black",
         #  size=18),
-        panel.grid.major = element_blank(),   #不显示网格线
-        panel.grid.minor = element_blank())+  #不显示网格线
-  ylab("Process importance")+xlab("developmental_stages") #设置x轴和y轴的标题
+        panel.grid.major = element_blank(),   
+        panel.grid.minor = element_blank())+  
+  ylab("Process importance")+xlab("developmental_stages") 
 P
-jpeg(file = "C:/Users/liujiale/Desktop/Pd.jpg",width =3000,height = 2000,units = "px",res =300)#结果保存
+jpeg(file = "C:/Users/liujiale/Desktop/Pd.jpg",width =3000,height = 2000,units = "px",res =300)
 print(P)
 dev.off()
 
